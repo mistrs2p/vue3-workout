69ff46ec1588ba2508e5a9f67f5776ded72ab418
mistRain <mahdimousavi40@gmail.com>
2022-05-04 22:07:08 +0430

    Style added

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -3,28 +3,65 @@
  -->
  <!-- find methods returns a value of the first element in a provided array that satisfied the provided testing function -->
 <template>
+  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+
+    <div class="post-list">
+      <div 
+        class="post" 
+        v-for="postId in thread.posts" 
+        :key="postId"
+      >
+        <div class="user-info">
+          <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
+
+          <a href="#">
+            <img
+              class="avatar-large"
+              :src="userById(postById(postId).userId).avatar"
+              alt=""
+            />
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>
+              {{ postById(postId).text }}
+            </p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">{{ postById(postId).publishedAt }}</div>
+
+      </div>
+     
+    </div>
+  </div>
+<!-- 
   <div>Hello from page home</div>
   <div v-for="thread in threads" :key="thread.id">
     <h2>{{ thread.title }}</h2>
-    <!-- <div v-for="postId in thread.posts" :key="postId">
+    <div v-for="postId in thread.posts" :key="postId">
       <p>{{ posts.find(post => post.id === postId).text}}</p>
       <p>{{ users.find(user => user.id === posts.find(post => post.id === postId).userId).name }}</p>
-    </div> -->
-
-    <!-- OR -->
-    <div v-for="postId in thread.posts" :key="postId">
-      <!-- <p>{{ userByPostId(postId).name }}</p> -->
-      <p>{{ userById(postById(postId).userId).name }}</p>
-      <p>{{ postById(postId).text}}</p>
     </div>
 
-  </div>
+    OR
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ userByPostId(postId).name }}</p>
+      <p>{{ userById(postById(postId).userId).name }}</p>
+      <p>{{ postById(postId).text }}</p>
+    </div>
+  </div> -->
 </template>
 
 <script>
 // @ symbol reffers to src location
 import dataSource from "@/data.json";
-console.log(dataSource)
+console.log(dataSource);
 export default {
   data() {
     return {
@@ -36,16 +73,18 @@ export default {
   },
   methods: {
     postById(postId) {
-      return this.posts.find(post => post.id === postId)
+      return this.posts.find((post) => post.id === postId);
     },
     // userById(userId) {
     //   return this.users.find(user => user.id === this.postById(userId).userId)
     // },
     userByPostId(postId) {
-      return this.users.find(user => user.id === this.postById(postId).userId)
+      return this.users.find(
+        (user) => user.id === this.postById(postId).userId
+      );
     },
     userById(userId) {
-      return this.users.find(user => user.id === userId)
+      return this.users.find((user) => user.id === userId);
     },
   },
 };
